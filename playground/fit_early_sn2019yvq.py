fde3a33505fb071d8e3119d1a1a673d5a2ad9b10
Adam Miller <amiller.astro@gmail.com>
2020-01-28 10:00:42 -0600

    BUG: path to filename

--- playground/fit_early_sn2019yvq.py
+++ playground/fit_early_sn2019yvq.py
@@ -8,7 +8,7 @@ f_unc_data = prep_df.f_unc_data.values
 fcqfid_data = prep_df.fcqfid_data.values
 
 fit_lc(t_data, f_data, f_unc_data, fcqfid_data,
-       mcmc_h5_file='/projects/p30796/ZTF/SN2019yvq/sn2019yvq_emcee.h5', 
+       mcmc_h5_file='/projects/p30796/ZTF/SN2019yvq/sn2019yvq_4d_emcee.h5', 
        max_samples=10000, 
        ncores=5,
        use_emcee_backend=True,
